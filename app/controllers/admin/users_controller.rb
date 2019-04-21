29126d7ee1be4ae73f103200bb8903830beb52e0
nargok <nargok@gmail.com>
2019-04-22 00:11:01 +0900

    [WIP]user index with bug

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,12 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -7,19 +15,30 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if !@user.save
-      redirect_to admin_user_pathm notice: "ユーザー「#{@user.name}」を登録しました。"
+      redirect_to admin_user_path notice: "ユーザー「#{@user.name}」を登録しました。"
     else
       render :new
     end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
-  def show
+  def update
+    @user = User.find(params[:id])
+
+    if @user.save
+      redirect_to admin_user_path notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :new
+    end
   end
 
-  def index
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_user_url, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
 
   private
