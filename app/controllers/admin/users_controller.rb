200111a8371d02da6e1debcbd934e66e0f3c7e5a
nargok <nargok@gmail.com>
2019-04-22 23:43:17 +0900

    bug fix users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,25 +11,25 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
 
-    if !@user.save
-      redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を登録しました。"
+    if @user.save
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
     else
       render :new
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
   def update
     @user = User.find(params[:id])
 
-    if @user.save
-      redirect_to admin_user_path notice: "ユーザー「#{@user.name}」を更新しました。"
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
     else
       render :new
     end
